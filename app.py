76af821af7418097a754b5ce7533b058e1fea823
kjh888 <142726396+kjh888@users.noreply.github.com>
2023-08-27 18:39:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
+
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -41,13 +43,53 @@ def callback():
 def handle_message(event):
     message = text=event.message.text
     #文字表達
-    if re.match('告訴我秘密',message):
-        line_bot_api.reply_message(event.reply_token,TextSendMessage('我是不會告訴你地!'))
+    if re.match('1451264564152156446',message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('error'))
     else:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('有什麼需要我幫忙的嗎???'))
+        #大圖片(內容包含:颱風情報、南台選課、 BMI值計算、食物熱量查詢)
+        carousel_template_message = ImagemapSendMessage(
+            base_url ="https://i.imgur.com/I9baIpi.jpg",
+            alt_text='需要什麼服務嗎?',
+            base_size=BaseSize(height=2000, width=2000),
+            actions=[
+                URIImagemapAction(
+                    #颱風
+                    link_uri="https://www.cwb.gov.tw/V8/C/P/Typhoon/TY_NEWS.html",
+                    area=ImagemapArea(
+                        x=0, y=0, width=1000, height=1000
+                    )
+                ),
+                URIImagemapAction(
+                    #熱量
+                    link_uri="http://211.21.168.52/FOOD/%A5D%AD%B9%C3%FE.htm",
+                    area=ImagemapArea(
+                        x=1000, y=0, width=1000, height=1000
+                    )
+                ),
+                URIImagemapAction(
+                    #BMI
+                    link_uri="https://depart.femh.org.tw/dietary/3OPD/BMI.htm",
+                    area=ImagemapArea(
+                        x=0, y=1000, width=1000, height=1000
+                    )
+                ),
+                URIImagemapAction(
+                    #南台選課
+                    link_uri="https://course.stust.edu.tw/CourSel/Pages/QueryAndSelect.aspx",
+                    area=ImagemapArea(
+                        x=1000, y=1000, width=1000, height=1000
+                    )
+                )
+            ]
+        )
+        line_bot_api.reply_message(event.reply_token,carousel_template_message)
+
+       
 
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
     
